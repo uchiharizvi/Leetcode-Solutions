15194786f942407d6dc2e7baa5fee297ba0aff53
Kavish Rizvi <rizvikavish205.com>
2022-08-12 10:03:31 +0530

    LC_EASY_21

--- src/easy/LC_014.java
+++ src/easy/LC_014.java
@@ -1,39 +1,31 @@
 package easy;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LC_014 {
     public static void main(String[] args) {
-        String[] values = {"dog,racecar,car"};
+        String[] values = {"wxyzw", "wlkjw", "wwwxxx"};
         System.out.println(longestCommonPrefix(values));
     }
 
+    /**
+     * Tc : 1ms
+     * Sc : 42.5 MB
+     */
     public static String longestCommonPrefix(String[] values) {
-        HashMap<Character, Integer> freq = new HashMap<>();
-        StringBuilder result = new StringBuilder();
-        for (String value : values) {
-            for (int i = 0; i < value.length(); i++) {
-                char x = value.charAt(i);
-                if (freq.containsKey(x)) freq.put(x, freq.get(x) + 1);
-                else freq.put(x, 1);
-            }
-        }
-        print(freq);
-        int maxFrequency = Collections.max(freq.values());
-        for (Map.Entry<Character, Integer> e : freq.entrySet()) {
-            if (e.getValue() == maxFrequency) {
-                Character x = e.getKey();
-                result.append(x);
-            }
-        }
-        return String.valueOf(result);
-    }
+        if (values.length == 0) return " ";
+        if (values.length == 1) return values[0];
+        Arrays.sort(values);
 
-    private static void print(HashMap<?, ?> freq) {
-        for (Map.Entry<?, ?> e : freq.entrySet()) {
-            System.out.println(e.getKey() + " : " + e.getValue());
+        int end = Math.min(values[0].length(), values[values.length - 1].length());
+        int i = 0;
+        while (i < end && values[0].charAt(i) == values[values.length - 1].charAt(i)) {
+            i++;
         }
+        String result = values[0].substring(0, i);
+        return result;
     }
 }
---
+++ src/easy/LC_20.java
@@ -0,0 +1,10 @@
+package easy;
+/**Valid Parenthesis*/
+public class LC_20 {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isValid(String s){
+        return false;
+    }
+}
